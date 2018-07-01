2a12c23c4c47333c6915d28b096f014abff731f4
devDonald <donaldebuga@gmail.com>
2018-07-01 23:59:41 +0100

    second commit

--- app/src/main/java/donald/com/journalapp/ViewJournal.java
+++ app/src/main/java/donald/com/journalapp/ViewJournal.java
@@ -2,12 +2,16 @@ package donald.com.journalapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ViewJournal extends AppCompatActivity {
     private DatabaseReference mAllJournalsDatabase;
@@ -15,6 +19,7 @@ public class ViewJournal extends AppCompatActivity {
     private FirebaseUser mUser;
     private String uID;
     private TextView mContent, mCategory, mDate;
+    private String position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +36,33 @@ public class ViewJournal extends AppCompatActivity {
         mAllJournalsDatabase = FirebaseDatabase.getInstance().getReference("My Journals").child(uID);
 
 
+        Bundle extras = getIntent().getExtras();
+        if (extras!=null) {
+            position = extras.getString("position");
+            if (position != null) {
+                DatabaseReference userRef = mAllJournalsDatabase.child(position);
+
+                Log.d("userref", "" + userRef);
+                ValueEventListener valueEventListener = new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // Log.d("ds",""+ds);
+
+                        mContent.setText(dataSnapshot.child("content").getValue(String.class));
+                        mCategory.setText(dataSnapshot.child("category").getValue(String.class));
+                        mDate.setText(dataSnapshot.child("date").getValue(String.class));
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                };
+                userRef.addListenerForSingleValueEvent(valueEventListener);
+
+
+            }
+
+        }
     }
 }
