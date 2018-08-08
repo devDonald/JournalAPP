30be665a771423fea2bb72e134d4ff1760f00d55
devDonald <donaldebuga@gmail.com>
2018-08-08 08:30:32 +0100

    final Commit

--- app/src/main/java/donald/com/journalapp/EditJournal.java
+++ app/src/main/java/donald/com/journalapp/EditJournal.java
@@ -33,5 +33,6 @@ public class EditJournal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_journal);
+    }
 
 }
